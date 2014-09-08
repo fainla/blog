a6a36e101b26f276af6752dbb88dd697e5b56297
fainla <ainlapoiss@hot.ee>
2014-09-08 14:59:13 +0300

    Rename welcome controller to posts

--- controllers/posts.php
+++ controllers/posts.php
@@ -1,9 +1,9 @@
 <?php
 
-welcome posts extends Controller{
+class posts extends Controller{
 
 	function index(){
-		$this->users = get_all("SELECT * FROM user");
+		$this->posts = get_all("SELECT * FROM posts");
 	}
 
     function index_ajax(){
@@ -15,4 +15,8 @@ welcome posts extends Controller{
 		echo "\$_POST:<br>";
 		var_dump($_POST);
 	}
+    function view(){
+        $post_id = $this->params[0];
+        $this->post = get_first("select * from post natural join user where post_id='$post_id'");
+    }
 }
--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,12 +1,11 @@
 
-<?foreach( $posts as $post ):?>
 
-    <h1><a href="<?=BASE_URL?>posts/view/<?=$posts['posts_id']?>"><?=$post['post_subject']?></a></h1>
+
+    <h1><?=$post['post_subject']?></h1>
     <p><?=$post['post_text']?></p>
         <div>
             <span class="badge">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label label-default">alice</span> <span class="label label-primary">story</span> <span class="label label-success">blog</span> <span class="label label-info">personal</span> <span class="label label-warning">Warning</span>
                 <span class="label label-danger">Danger</span></div>
         </div>
 
-<?endforeach?>
 
