9cf14573d5993425dc3951e0ca8c4424fd173685
fainla <ainlapoiss@hot.ee>
2014-09-24 13:49:53 +0300

    blog
    commit 24.09

--- views/users/users_index.php
+++ views/users/users_index.php
@@ -4,3 +4,16 @@
         <li class="list-group-item"><a href="users/view/<?= $user['user_id'] ?>/<?= $user['username'] ?>"><?=$user['username']?></a></li>
     <? endforeach ?>
 </ul>
+
+<form role="form">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Username</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" name="data[username]">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="data[password]">
+    </div>
+
+    <button type="submit" class="btn btn-default">Add user</button>
+</form>
