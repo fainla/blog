f7887afdac48ceec19125c9f90e796de31ef5b42
fainla <ainlapoiss@hot.ee>
2014-09-24 17:13:39 +0300

    User name, password

--- views/users/users_index.php
+++ views/users/users_index.php
@@ -5,15 +5,3 @@
     <? endforeach ?>
 </ul>
 
-<form role="form">
-    <div class="form-group">
-        <label for="exampleInputEmail1">Username</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" name="data[username]">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="data[password]">
-    </div>
-
-    <button type="submit" class="btn btn-default">Add user</button>
-</form>
